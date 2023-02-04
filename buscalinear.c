472e5ba6b1fc6d930fc6a9e0777e445309c3c476
Anderson Fernandes <95195152+AndersonBF@users.noreply.github.com>
2023-02-04 01:27:08 -0300

    Update buscalinear.c

--- buscalinear.c
+++ buscalinear.c
@@ -9,20 +9,21 @@ int main () {
     int n,i, vetor[TAMANHOMAXIMO], numero;
     printf("quantos numeros o vetor tem?");
     scanf("%d", &n);
-
+//preenche o vetor com os dados inseridos pelo usuario
     for(i =0;i<n;i++) {
         scanf("%d", &vetor[i]);
     }
     printf("vetor:");
+//mostra o vetor preenchido 
     for (i = 0; i < n; i++) {
     printf("%d ", vetor[i]);
   }
     printf("qual numero quer buscar?");
     scanf("%d", &numero);
 
-    for (int i=0;i<n;i++) {
+    for (int i=1;i<n;i++) {
         if(numero==vetor[i]) {
-            printf ("o numero está no vetor na posiçao %d\n", i+1);
+            printf ("o numero está no vetor na posiçao %d\n", i);
         }
     }
 
